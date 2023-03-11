5959f05f55fbd25c0f63e1127e8e23a4e10ea655
3ilib0ba <evonniy@mail.ru>
2023-03-11 23:08:24 +0300

    lab-1-done

--- Lab-1/lab-1.sql
+++ Lab-1/lab-1.sql
@@ -15,10 +15,10 @@ $$
         column_index   text;
         result         text;
     begin
-        raise notice 'Пользователь: Ivan Ivanov (s100000)';
-        raise notice 'Таблица: %', current_setting('myvars.tab');
-        raise notice 'No  Имя столбца    Атрибуты';
-        raise notice '--- -------------- ------------------------------------------';
+        raise notice '|Пользователь: Ivan Ivanov (s100000)';
+        raise notice '|Таблица: %', current_setting('myvars.tab');
+        raise notice '|No  Имя столбца    Атрибуты';
+        raise notice '|--- -------------- ------------------------------------------';
         select "oid" into table_id from ucheb.pg_catalog.pg_class where "relname" = current_setting('myvars.tab');
         for column_record in select * from ucheb.pg_catalog.pg_attribute where attrelid = table_id
             loop
@@ -27,13 +27,6 @@ $$
                 my_column_name = column_record.attname;
                 column_type_id = column_record.atttypid;
                 select typname into column_type from ucheb.pg_catalog.pg_type where oid = column_type_id;
-
-                if column_record.atttypmod != -1 then
-                    column_type = column_type || ' (' || column_record.atttypmod || ')';
-                    if column_type = 'int4' then
-                        column_type = 'NUMBER';
-                    end if;
-                end if;
 
                 if column_record.attnotnull then
                     column_type = column_type || ' Not null';
@@ -52,17 +45,16 @@ $$
                 and inf.column_name = my_column_name
                 and indexdef ~ (my_column_name)
                 into column_index;
-                column_index = '"' || column_index || '"';
 
-                select format('%-3s %-14s %-8s %-2s %s', column_number, my_column_name, 'Type', ':', column_type)
+                select format('%-3s %-14s %-6s %-2s %s', column_number, my_column_name, 'Type', ':', column_type)
                 into result;
-                raise notice '%', result;
+                raise notice '%', '|' || result;
 
                 if length(column_comment) > 0 then
-                    select format('%-18s %-8s %-2s %s', '""', 'Constr', ':', column_comment) into result;
-                    raise notice '%', result;
+                    select format('%25s %-2s %s', 'Constr', ':', column_comment) into result;
+                    raise notice '%', '|' || result;
                 end if;
             end if;
         end loop;
-    end;
+    end
 $$ LANGUAGE plpgsql;
